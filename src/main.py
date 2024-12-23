0568fdcc5665ac40c502a1f976da9c1b4379fc74
rachraines <rachraines97@gmail.com>
2024-12-23 11:18:39 -0500

    texnode.py updated and prints in main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    # create a new TextNode object with sample values
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+main()
