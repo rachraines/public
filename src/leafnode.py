a7c112cfba6a028e90a9fa326f5f0cbf048a7271
rachraines <rachraines97@gmail.com>
2024-12-30 10:22:33 -0500

    LeafNode.py updated with LeafNode constructor class

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,1 +1,11 @@
-from htmlnode import 
+from htmlnode import HTMLNode
+
+# Creates LeafNode as a child class inheriting from HTMLNode
+class LeafNode(HTMLNode):
+    # Inherits tags, value, and props (no children)
+    def __init__(self, tag=None, value=None, props=None):
+        # Value cannot be none
+        if value is None:
+            raise ValueError("A LeafNode must have a value")
+        # Inherits all inputs from the super class but sets children automatically to None.
+        super().__init__(tag=tag, value=value, children=None, props=props)
