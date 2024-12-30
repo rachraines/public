69bfa365f9679a0327f20dd643a71250556e5df0
rachraines <rachraines97@gmail.com>
2024-12-30 11:19:09 -0500

    more tests added for LeafNode.py

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -14,9 +14,6 @@ class TestLeafNode(unittest.TestCase):
     # tests LeafNode instance with default values
     def test_initialization_defaults(self):
         node = LeafNode()
-        self.assertIsNone(node.tag)
-        self.assertIsNone(node.children)
-        self.assertIsNone(node.props)
         with self.assertRaises(ValueError):
             LeafNode()
 
@@ -25,3 +22,19 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("a", "Click me!", props={"href": "https://google.com"})
         expected_string = "<a href='https://google.com'>Click me!</a>"
         self.assertEqual(node.to_html(), expected_string)
+
+    # tests to_html with no props
+    def test_to_html_no_props(self):
+        node = LeafNode("a", "Click me!")
+        expected_string = "<a>Click me!</a>"
+        self.assertEqual(node.to_html(), expected_string)
+
+    # tests to_html with no tag
+    def test_to_html_no_tag(self):
+        node = LeafNode(value="Click me!")
+        self.assertEqual(node.to_html(), "Click me!")
+
+    # test to_html with no value
+    def test_to_html_no_value(self):
+        with self.assertRaises(ValueError):
+            LeafNode(tag="p")
