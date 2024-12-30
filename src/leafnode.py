4541bad0cde7125852c24b1aee54b93c70b46dcc
rachraines <rachraines97@gmail.com>
2024-12-30 10:45:20 -0500

    to_html method created for LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -4,8 +4,18 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     # Inherits tags, value, and props (no children)
     def __init__(self, tag=None, value=None, props=None):
+        
         # Value cannot be none
         if value is None:
             raise ValueError("A LeafNode must have a value")
+        
         # Inherits all inputs from the super class but sets children automatically to None.
         super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        # Value cannot be none
+        if self.value is None:
+            raise ValueError("A LeafNode must have a value")
+        elif self.tag is None:
+            return self.value
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
