44e1c00fc9d951f7b6b696cbb06013033387400a
rachraines <rachraines97@gmail.com>
2025-01-01 10:24:50 -0500

    text_to_html.py created

---
+++ src/text_to_html.py
@@ -0,0 +1,21 @@
+from leafnode import LeafNode
+from textnode import TextNode, TextType
+
+# converts textnode to HTMLnode
+def text_node_to_html(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise ValueError("Invalid TextType")
+    elif text_node.text_type == TextType.TEXT:
+        return LeafNode(value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text or "alt text"})
+    else:
+        raise NotImplementedError(f"HTML conversion for {text_node.text_type} is not implented.")
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 # acceptable text types
 class TextType(Enum):
@@ -29,3 +30,22 @@ class TextNode:
     # creates a string representation of the TextNode
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    # converts textnode to HTMLnode
+    def text_node_to_html(text_node):
+        if not isinstance(text_node.text_type, TextType):
+            raise ValueError("Invalid TextType")
+        elif text_node.text_type == TextType.TEXT:
+            return LeafNode(value=text_node.text)
+        elif text_node.text_type == TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        elif text_node.text_type == TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text)
+        elif text_node.text_type == TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        elif text_node.text_type == TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        elif text_node.text_type == TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text or "alt text"})
+        else:
+            raise NotImplementedError(f"HTML conversion for {text_node.text_type} is not implented.")
